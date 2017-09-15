bb1299bdaf9c3b8526dbd6dee5692172675bf3b9
Nav <navin.brohier@gmail.com>
2017-09-15 17:03:35 +1000

    +Addition to spritesheet class object

--- JS/SpriteUtil.js
+++ JS/SpriteUtil.js
@@ -2,14 +2,11 @@ var SpriteUtility = function()
 {
 
     this.m_Initialized = false;
-    this.m_spritesheet = "";
 
-    this.m_TileWidth = 0;
-    this.m_TileHeight = 0;
-    this.m_ScaleX = m_TileWidth;
-    this.m_ScaleY = m_TileHeight;
-    this.m_Row = 0;
-    this.m_Col = 0;
+    this.m_ScaleX = 1;
+    this.m_ScaleY = 1;
+    this.m_PosX = 0;
+    this.m_posY = 0;
 
 
 
@@ -18,6 +15,25 @@ var SpriteUtility = function()
 
     }
 
+// Sprite Sheet data
+    this.SpriteSheet = function()
+    {
+        this.m_SpritePath = "";
+        this.m_spritesheet = document.createElement("img");
+        this.m_spritesheet.src = this.m_SpritePath; 
+        this.m_TileWidth = 0;
+        this.m_TileHeight = 0;
+        this.m_Row = 0;
+        this.m_Col = 0;
+
+        this.m_Initialize = function(a_Filepath,a_Tilewidth,a_TileHeight,a_Rows,a_Columns)
+        {
+            
+        }
+    }
+
+
+// SpriteRender.frame class
     this.Frame = function()
     {
         this.m_TileWidth = 0;
@@ -25,17 +41,32 @@ var SpriteUtility = function()
         this.m_Row = 0;
         this.m_Col = 0;
 
-        this.prototype.createFrames = function()
+        this.createFrame = function()
         {
 
         }
     }
 
-    this.anim = 
+//SpriteRender.Anim class
+    this.anim = function()
+    {
+
+        this.SetupAnim = function()
+        {
+
+        }
+    }
 
     this.RenderSprite = function(DeltaTime,Frames)
     {
-        context.drawImage(m_spritesheet,m_Row,m_Col,m_TileWidth,m_TileHeight,m_ScaleX,m_ScaleY);
+        try
+        {
+            context.drawImage(this.m_spritesheet,this.m_Row,this.m_Col,this.m_TileWidth,this.m_TileHeight,this.m_PosX,this.m_PosY,this.m_ScaleX,this.m_scaleY);
+        }
+        catch(err)
+        {
+            document.write("Render Sprite function has encountered an error. Please check variables.");
+        }
     }
 
 };
